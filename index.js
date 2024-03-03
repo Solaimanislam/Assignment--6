518fb2df164a050a9902ab111bcc85074095b307
Solaimanislam <islamsi2001@gmail.com>
2024-03-03 10:56:03 +0600

    added latest posts

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const allPost = async() => {
                             <div class="grid w-32 h-32 bg-base-300 rounded-xl place-items-center"><img class=" rounded-xl" src="${post.image} " alt=""> </div>
                           </div>
                       <div>
-                        <div class="font-inter flex gap-8">
+                        <div class="font-inter lg:mt-8 flex gap-8">
                             <h6>#${post.category} </h6>
                             <p>Author: ${post.author.name} </p>
                         </div>
@@ -24,7 +24,7 @@ const allPost = async() => {
                         <p class="py-6 font-inter">${post.description} </p>
                         <hr class="border-dashed text-xl mb-6">
                         <div class="flex items-center justify-between">
-                            <div class="flex gap-4">
+                            <div class="flex lg:mb-8 gap-4">
                                 <div class="flex items-center gap-4">
                                     <img src="images/sms.png" alt="">
                                     <p>${post.comment_count}</p>
@@ -50,4 +50,37 @@ const allPost = async() => {
     })
 }
 
-// allPost();
+const latestPost = async() => {
+    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts');
+    const data = await res.json();
+    // console.log(data);
+    const latestPost = document.getElementById('latest-post');
+    data.forEach((item) => {
+        // console.log(item);
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="card card-compact w-96 bg-base-100 shadow-xl">
+                    <figure><img src="${item.cover_image} " alt="Shoes" /></figure>
+                    <div class="card-body">
+                        <div class="text-[#12132D99] flex gap-2 items-center">
+                            <img src="images/date.png" alt="">
+                            <p class="text-base">${item?.author?.posted_date? item.author.posted_date: 'No publish date'} </p>
+                        </div>
+                      <h2 class="card-title text-[#12132D] font-extrabold text-lg">${item.title} </h2>
+                      <p class="text-[#12132D99]">${item.description.slice(0, 100)} </p>
+                      <div class="flex gap-4 items-center">
+                        <img class=" w-12 h-12 rounded-full" src="${item.profile_image} " alt="">
+                        <div>
+                            <h4 class="font-bold text-base">${item.author.name} </h4>
+                            <p class="text-[#12132D99]">${item?.author?.designation? item.author.designation: 'Unknown'}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+         `;
+         latestPost.appendChild(div);
+    })
+}
+
+allPost();
+latestPost();
